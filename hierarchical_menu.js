dedd69d55e0318a9a1c29cd50eb446874bdbb17b
Yuliya Batyuk <yuliyabat@gmail.com>
2017-08-06 17:39:54 -0500

    Update hierarchical_menu.js

--- hierarchical_menu.js
+++ hierarchical_menu.js
@@ -1,12 +1,27 @@
-$(function(){
+/**
+ * hMenu - jQuery plugin for Horizontal Hierarchical Menu.
+ * @version v1.0.0
+ * @link https://github.com/yuliyabat/horizontal-hierarchical-menu
+ * @license MIT
+ */
+(function($){
+
+	"use strict";
 	
-	$(".h-menu li").hover(
-		function(){
-			$(this).find("ul:first").slideDown(300);
-			},
-		function(){
-			$(this).find("ul:first").slideUp(300);
-			}
-	);
+	$.fn.hMenu = function (options) {
+		var base = this,
+		    settings = $.extend({
+		    	slideSpeed : 300
+
+		    }, options ); 
+		$(base).find('li').hover(
+			function(){
+				$(this).find("ul:first").slideDown(settings.slideSpeed);
+				},
+			function(){
+				$(this).find("ul:first").slideUp(settings.slideSpeed);
+				}
+		);
+    }
 		
-});
+}(jQuery));
